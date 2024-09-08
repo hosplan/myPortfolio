47524f16eabdb20270880185aa075b83b2fe017c
김태호 <>
2024-09-08 23:06:39 +0900

    gh-pages 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  assetPrefix:
+  process.env.NODE_ENV === "production"
+    ? "https://hosplan.github.io/myPortfolio"
+    : "",
+};
+
 
 export default nextConfig;
